803120e417f3f1fc3e9767d0c724a93279f4e3ba
jamesmulvaney <jamesmulvaney@outlook.com>
2023-09-30 20:08:53 +0100

    Changed maintenance to use subcommands

--- slash-commands/maintenance.js
+++ slash-commands/maintenance.js
@@ -13,76 +13,53 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("maint")
     .setDescription("Add or adjust maintenance times.")
-    .addStringOption((option) =>
-      option
-        .setName("type")
-        .setDescription("Add or Adjust")
-        .setRequired(true)
-        .addChoices(
-          { name: "Add", value: "add" },
-          { name: "Adjust", value: "adjust" }
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("add")
+        .setDescription("Add maintenance times")
+        .addStringOption((option) =>
+          option
+            .setName("start")
+            .setDescription("Start time in ISO format")
+            .setRequired(true)
+        )
+        .addStringOption((option) =>
+          option
+            .setName("end")
+            .setDescription("End time in ISO format")
+            .setRequired(true)
         )
     )
-    .addStringOption((option) =>
-      option
-        .setName("start")
-        .setDescription("Start time in ISO format.")
-        .setRequired(false)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("end")
-        .setDescription("End time in ISO format.")
-        .setRequired(false)
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("adjust")
+        .setDescription("Adjust maintenance end time")
+        .addStringOption((option) =>
+          option
+            .setName("end")
+            .setDescription("End time in ISO format")
+            .setRequired(true)
+        )
     )
     .setDMPermission(false),
   async execute(interaction) {
-    const type = interaction.options.getString("type");
-    const start = interaction.options.getString("start");
-    const end = interaction.options.getString("end");
+    if (interaction.options.getSubcommand() === "add") {
+      const start = interaction.options.getString("start");
+      const end = interaction.options.getString("end");
 
-    //Validation
-    //Both types require an end time
-    if (!end) {
-      await interaction.reply(
-        `You must specify an end time when using \`${type}\``
-      );
-
-      return;
-    }
-
-    if (!dayjs(end).isValid()) {
-      await interaction.reply(
-        `Invalid ISO time string provided. Current ISO time \`${dayjs()
-          .utc()
-          .toISOString()}\``
-      );
-
-      return;
-    }
-
-    //Add or Adjust
-    if (type === "add") {
-      //Validation
-      if (!start) {
-        await interaction.reply(
-          `You must specify a start time when using \`add\``
-        );
-
-        return;
-      }
-
-      if (!dayjs(start).isValid()) {
-        await interaction.reply(
-          `Invalid ISO time string provided. Current ISO time \`${dayjs()
+      //Date Validation
+      if (!dayjs(start).isValid() || !dayjs(end).isValid()) {
+        await interaction.reply({
+          content: `Invalid ISO time string provided. Current ISO time \`${dayjs()
             .utc()
-            .toISOString()}\``
-        );
+            .toISOString()}\``,
+          ephemeral: true,
+        });
 
         return;
       }
 
-      //Add maint times to db
+      //Add times to db
       try {
         const newConfig = await setMaintenanceTime(
           dayjs(start).utc().toDate(),
@@ -92,17 +69,32 @@ module.exports = {
         config[0] = newConfig;
 
         await interaction.reply(
-          `Maintenance added. Starting at \`${start}\` and ending at \`${end}\``
+          `New maintenance added! Starting at \`${start}\` and ending at \`${end}\``
         );
       } catch (err) {
         console.error(err);
       }
-    } else if (type === "adjust") {
-      //Validation
+    } else if (interaction.options.getSubcommand() === "adjust") {
+      const end = interaction.options.getString("end");
+
+      //Date Validation
+      if (!dayjs(end).isValid()) {
+        await interaction.reply({
+          content: `Invalid ISO time string provided. Current ISO time \`${dayjs()
+            .utc()
+            .toISOString()}\``,
+          ephemeral: true,
+        });
+
+        return;
+      }
+
+      //Only adjust if there is a scheduled maintenance
       if (!config[0].maintStart) {
-        await interaction.reply(
-          `There is no maintenance set. Use the \`add\` option to add maintenance times.`
-        );
+        await interaction.reply({
+          content: `There is no maintenance set. Use \`/maint add\` to add maintenance times.`,
+          ephemeral: true,
+        });
 
         return;
       }
